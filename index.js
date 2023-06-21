161adf6bf6f9c828f329a19d382cc4f52fbfde6f
GURRAM GAYATRI NAGA NANDINI <94378783+gayatrinaganandini@users.noreply.github.com>
2023-06-21 13:36:18 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,26 +1,45 @@
-const express = require("express");
-const axios = require("axios");
-const app = express();
-app.get("/numbers", async (req, res) => {
-  res.send(req.params);
-  const urls = req.query.url;
-  const numbers = [];
-  for (const url of urls) {
-    try {
-      const response = await axios.get(url);
-      if (response.status === 200) {
-        const data = response.data;
-        if (data.number) {
-          numbers.push(data.number);
-        }
-      }
-    } catch (error) {
-      console.error(error);
-    }
-  }
-  res.json(numbers);
-});
+const axios = require('axios');
 
-app.listen(8008, () => {
-  console.log("Server running on port 8008");
-});
+async function fetchNumbersFromURL(url) {
+  try {
+    const response = await axios.get(url);
+    if (response.data && Array.isArray(response.data.numbers)) {
+      return response.data.numbers;
+    }
+  } catch (error) {
+    console.error(`Failed to fetch numbers from ${url}:`, error.message);
+  }
+  return [];
+}
+
+async function mergeUniqueNumbersFromURLs(urls) {
+  const uniqueNumbers = new Set();
+
+  const requests = urls.map(url => fetchNumbersFromURL(url));
+
+  try {
+    const responses = await Promise.all(requests);
+    responses.forEach(numbers => {
+      numbers.forEach(number => uniqueNumbers.add(number));
+    });
+  } catch (error) {
+    console.error('Failed to fetch numbers:', error.message);
+  }
+
+  return Array.from(uniqueNumbers).sort((a, b) => a - b);
+}
+
+// Test case
+const testURLs = [
+  'http://104.211.219.98/numbers/primes',
+  'http://104.211.219.98/numbers/fibo',
+  'http://104.211.219.98/numbers/odd'
+];
+
+mergeUniqueNumbersFromURLs(testURLs)
+  .then(numbers => {
+    console.log({ numbers });
+  })
+  .catch(error => {
+    console.error('Error:', error.message);
+  });
